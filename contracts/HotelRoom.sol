f81aca32c037bb6ca0d313074a1269e548f79b14
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-02-16 08:35:10 +0545

    function definitions done

--- contracts/HotelRoom.sol
+++ contracts/HotelRoom.sol
@@ -1,6 +1,30 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0
+pragma solidity ^0.8.0;
 
+//ether payments
+//modifiers
+//visibility
+//events
+//enums
+
+
+//booking a hotel room and payment with cryptocurrency
 contract HotelRoom{ 
-     
-}
+    //vacant
+    //occupied... to track this, we use enum
+
+    enum Statuses { Vacant, Occupied}; // vacant and occupied are two options for enum(datatype) Status
+    Statuses currentSatus;
+
+    address payable public owner;   //eth address of the hotel owner
+
+    constructor(){ 
+        owner= msg.sender;
+        currentStatus= Statuses.Vacant;
+    }
+    function bookRoom() payable{        //payable rakhepaxi ether transactions ko code garna milxa
+        //first we pay the owner of the hotel room
+        currentStatus = Statuses.Occupied
+        owner.transfer(msg.value);   //transfer function sends crypto to the recepient i.e. owner 
+     }
+} 
